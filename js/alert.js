705f7c7c1ace03892da952a7b888e0b0861ebbb5
bin <mso5382@naver.com>
2022-12-09 09:21:52 +0900

    html + css + img upload

---
+++ js/alert.js
@@ -0,0 +1,11 @@
+const phoneNum = document.querySelector(".phone");
+const email = document.querySelector(".email");
+
+phoneNum.addEventListener("click", () =>{
+  alert("010-0000-0000");
+});
+
+email.addEventListener("click", () => {
+  alert("newmso5382@gmail.com");
+});
+
